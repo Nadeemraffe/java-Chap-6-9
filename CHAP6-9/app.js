dd74218a9812ebb1efcfbf688dbba82f8c2caffe
Nadeemraffe <123829445+Nadeemraffe@users.noreply.github.com>
2023-02-05 08:52:40 -0800

    Add files via upload

---
+++ CHAP6-9/app.js
@@ -0,0 +1,83 @@
+// /Chap 6-9
+
+//Question 1
+
+// var num = 5;
+// document.write("<h3>Result</h3>");
+// document.write("<p>The value of num is 5</p>");
+// num = ++num;
+// document.write("<p>The value of ++num is 6</p>");
+// document.write("<p>Now the value of num is 6</p>");
+// num = num++;
+// document.write("<p>Value of num++ is 6</p>");
+// document.write("<p>Now the value of num is 7</p>");
+// num = --num;
+// document.write("<p>Value of --num is 6</p>");
+// document.write("<p>Now the value is 6</p>");
+// num = num--;
+// document.write("<p>Value of num-- is 6</p>");
+// document.write("<p>Now the value is 5</p>");
+
+//Question 2
+
+// var a = 2;
+// var b = 1;
+// var result = --a;
+// document.write("<p>The value of a is 1</p>");
+// result = --a - --b;
+// document.write("<p>The value of a is 0 and b is 0</p>")
+// result = --a - --b + ++b;
+// document.write("<p>The value of a is -1 and b is 1</p>");
+// result = --a - --b + ++b + b--;
+// document.write("<p>The value of a is -2 and b is 1</p>");
+// document.write("<p>The result is -1</p>");
+
+//Question 3
+
+// alert("Hello User");
+// prompt("Enter your name");
+
+//Question 5
+
+// var table = prompt("Enter a number","5");
+// document.write("<p>5x1=5</p>");
+// document.write("<p>5x2=10</p>");
+// document.write("<p>5x3=15</p>");
+// document.write("<p>5x4=20</p>");
+// document.write("<p>5x5=25</p>");
+// document.write("<p>5x6=30</p>");
+// document.write("<p>5x7=35</p>");
+// document.write("<p>5x8=40</p>");
+// document.write("<p>5x9=45</p>");
+// document.write("<p>5x10=50</p>");
+
+//Question 6
+
+// var subject1 = prompt("Enter Subject 1");
+// var subject2 = prompt("Enter Subject 2");
+// var  subject3 = prompt("Enter Subject 3");
+// var totalMarks = 100;
+// var marksObtainedSub1 = prompt("Enter Marks Obtained For Subject 1");
+// var marksObtainedSub2 = prompt("Enter Marks Obtained For Subject 2");
+// var marksObtainedSub3 = prompt("Enter Marks Obtainned For Subject 3");
+// var percentageSub1 = marksObtained1 / totalMarks * 100;
+// var percentageSub2 = marksObtained2 / totalMarks * 100;
+// var percentageSub3 = marksObtained3 / totalMarks * 100;
+// var sumTotalMarks = 300;
+// var sumOfMarks = marksObtainedSub1 + marksObtainedSub2 + marksObtainedSub3;
+// var sumPercentage = sumOfMarks / sumTotalMarks * 100;
+// document.write(subject1);
+// document.write(totalMarks);
+// document.write(marksObtainedSub1);
+// document.write(percentageSub1);
+// document.write(subject2);
+// document.write(totalMarks);
+// document.write(marksObtainedSub2);
+// document.write(percentageSub2);
+// document.write(subject3);
+// document.write(totalMarks);
+// document.write(marksObtainedSub3);
+// document.write(percentageSub3);
+// document.write(sumTotalMarks);
+// document.write(sumOfMarks);
+// document.write(sumPercentage);
